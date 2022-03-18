169820c423df55dad772b4195c035050313536ac
hadi <65118892+hadiazt@users.noreply.github.com>
2022-03-18 12:18:18 +0330

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,24 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js')
+const { basecolor } = require('../data/config.json')
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('help')
+        .setDescription('SoSiS Help Menu'),
+    async execute(interaction, client) {
+
+        const helpmsg = new MessageEmbed()
+            .setColor(basecolor)
+            .setTitle(`${client.user.username} Help Panel :`)
+            .setDescription(`
+**<:bell_emoji:914129896958205982> General Help :**
+<:space:874678195843125278><:right:874690882417360986>`+ '`' + '/' + '`' + `
+
+**<:information_emoji:914129757195620402> Other Help :**
+<:space:874678195843125278><:right:874690882417360986>`+ '`' + '/about' + '`' + `
+`)
+
+        return interaction.reply({ embeds: [helpmsg] });
+    },
+};
