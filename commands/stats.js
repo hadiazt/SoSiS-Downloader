03e84a293df7c7b1db6e2017cff3580e9f28867f
hadi <65118892+hadiazt@users.noreply.github.com>
2022-03-18 19:17:13 +0330

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -1,6 +1,8 @@
 const { SlashCommandBuilder } = require('@discordjs/builders')
 const { MessageEmbed } = require('discord.js')
 const { basecolor } = require('../data/config.json')
+const { Database } = require('beta.db')
+const db = new Database('./data/config.json')
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -24,6 +26,8 @@ module.exports = {
 <:space:874678195843125278><:bustsinsilhouette_emoji:914504068020437012> *Members Count : `+ '`' + MemberCount + '`' + `*
 
 <:space:874678195843125278><:keycapnumbersign_emoji:914500272422789161> *Channels Count : `+ '`' + client.channels.cache.size + '`' + `*
+
+<:space:874678195843125278><:counter_emoji:915916802855407656> *Commands Usage : `+ '`' + db.get('USAGE') + '`' + `*
 
 <:space:874678195843125278><:horizontaltrafficlight_emoji:914504404818874389> *Status : `+ '`' + client.user.presence.status + '`' + `*
 
