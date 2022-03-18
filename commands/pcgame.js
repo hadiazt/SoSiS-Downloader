29231e3c515909a07e7f3947b24fd9195ca937b5
hadi <65118892+hadiazt@users.noreply.github.com>
2022-03-18 12:57:31 +0330

    Update pcgame.js

--- commands/pcgame.js
+++ commands/pcgame.js
@@ -5,10 +5,18 @@ const { PCGame } = require('medias-info')
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('pcgame')
-        .setDescription('Search Your Entry On PC Category'),
-
+        .setDescription('Search Your Entry On PC Category')
+        .addStringOption(option =>
+            option.setName('enrty')
+                .setDescription('Your Entry For Search')
+                .setRequired(true)
+        ),
     async execute(interaction, client) {
+        var name = interaction.options.get('enrty').value;
 
+        PCGame({ name }).then(res => {
+            console.log(res)
+        })
 
         return interaction.reply({})
     },
